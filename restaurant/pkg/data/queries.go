e17da612f2c7b505120ab9d79bc0a8eeb3876f17
miladouski <70945893+miladouski@users.noreply.github.com>
2021-04-21 10:44:44 +0300

    task3_4

---
+++ restaurant/pkg/data/queries.go
@@ -0,0 +1,7 @@
+package data
+
+const readAllOrdersQuery = `SELECT orders.id, orders.date, orders.table_number, waiters.full_name, orders.price, orders.payment FROM "orders" RIGHT JOIN waiters on waiters.waiter_id = orders.waiter_id`
+const readOrdersQuery = `SELECT orders.id, orders.date, orders.table_number, waiters.full_name, orders.price, orders.payment FROM "orders" RIGHT JOIN waiters on waiters.waiter_id = orders.waiter_id WHERE orders.id = $1`
+const createOrderQuery = `INSERT INTO "orders" VALUES ($1, $2, $3, $4, $5, $6)`
+const updateOrderQuery = `UPDATE "orders" SET "payment"=$1 WHERE orders.id = $2`
+const deleteOrderQuery = `DELETE FROM "orders" WHERE orders.id = $1`
